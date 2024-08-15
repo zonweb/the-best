2823f0118f853def557842f35f5d82fbb08a7eb7
Dorra <140128615+dorra-web@users.noreply.github.com>
2024-08-15 13:51:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@
                 "top-houston-maritime-attorney-protecting-your-rights-at-sea.html",
                 "houston-maritime-attorney-services-your-trusted-legal-partner.html",
                 "experienced-houston-maritime-attorney-advocating-for-seafarers-rights.html",
-                "",
-                "",
-                "",
-                "",
-                "",
-                ""
+                "houston-maritime-attorney-comprehensive-legal-solutions-for-maritime-cases.html",
+                "your-guide-to-choosing-the-right-houston-maritime-attorney.html",
+                "houston-maritime-attorney-ensuring-justice-for-maritime-workers.html",
+                "navigating-maritime-law-insights-from-a-houston-maritime-attorney.html",
+                "houston-maritime-attorney-dedicated-to-your-maritime-legal-needs.html",
+                "protect-your-interests-with-a-leading-houston-maritime-attorney.html"
             ];
 
             // Generate indeks random berdasarkan panjang array pages
